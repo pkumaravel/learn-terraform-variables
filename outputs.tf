621b2012d10f3f754e6d2ae5ee8303d3789b520e
TEK Student <TEKstudent@vlab.lan>
2025-10-08 11:15:23 -0700

    added S3 ARN in output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "public_dns_name" {
   description = "Public DNS names of the load balancer for this project"
   value       = module.elb_http.this_elb_dns_name
 }
+
+output "bucket_arn" {
+  description = "The ARN of the S3 bucket."
+  value       = module.s3_bucket.bucket_arn
+}
