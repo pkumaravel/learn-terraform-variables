4800fa43dc36d6e7074a4577ceeb147381beddb5
TEK Student <TEKstudent@vlab.lan>
2025-10-08 11:00:43 -0700

    added s3 bucket module from provate repo

--- main.tf
+++ main.tf
@@ -127,3 +127,9 @@ module "ec2_instances" {
     environment = "development"
   }
 }
+
+module "s3_bucket" {
+  source  = "app.terraform.io/policy-as-code-training/s3-bucket-kumaravel/aws"
+  version = "1.0.0"
+  bucket_name = "my-bucket-kumaravel-unique"
+}
